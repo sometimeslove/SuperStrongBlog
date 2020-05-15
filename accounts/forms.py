4bcad4dbd11cf3975051356ddeda5b3bf61f6a96
sometimeslove <857508399@qq.com>
2020-05-15 17:30:05 +0800

    注册用户时增加家庭住址字段

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,6 +15,7 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.forms import widgets
 from django.conf import settings
+from django import forms
 from django.contrib.auth import get_user_model
 
 
@@ -27,16 +28,24 @@ class LoginForm(AuthenticationForm):
 
 
 class RegisterForm(UserCreationForm):
+    # home_address = forms.CharField(
+    #     label="家庭地址",
+    #     strip=False,
+    #     widget=widgets.TextInput(attrs={'placeholder': "家庭地址", "class": "form-control"}))
     def __init__(self, *args, **kwargs):
         super(RegisterForm, self).__init__(*args, **kwargs)
 
-        self.fields['username'].widget = widgets.TextInput(attrs={'placeholder': "username", "class": "form-control"})
-        self.fields['email'].widget = widgets.EmailInput(attrs={'placeholder': "email", "class": "form-control"})
-        self.fields['password1'].widget = widgets.PasswordInput(
-            attrs={'placeholder': "password", "class": "form-control"})
-        self.fields['password2'].widget = widgets.PasswordInput(
-            attrs={'placeholder': "repeat password", "class": "form-control"})
+        self.fields['username'].widget = widgets.TextInput(attrs={'placeholder': "用户名", "class": "form-control"})
+        self.fields['email'].widget = widgets.EmailInput(attrs={'placeholder': "邮箱", "class": "form-control"})
+        # self.fields['password1'].widget = widgets.PasswordInput(
+        #     attrs={'placeholder': "请输入密码", "class": "form-control"})
+        # self.fields['password2'].widget = widgets.PasswordInput(
+        #     attrs={'placeholder': "请再次输入密码", "class": "form-control"})
+        self.fields['home_address'].widget = widgets.TextInput(attrs={'placeholder': "家庭地址", "class": "form-control"})
+        self.fields['home_address'].widget = widgets.TextInput(attrs={'placeholder': "家庭地址", "class": "form-control"})
+
 
     class Meta:
         model = get_user_model()
-        fields = ("username", "email")
+        fields = ("username", "email","home_address","nickname")
+        # exclude = []
--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.urls import reverse
 from DjangoBlog.utils import get_current_site
 from django.utils.timezone import now
-
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your models here.
 
@@ -12,6 +13,7 @@ class BlogUser(AbstractUser):
     mugshot = models.ImageField('头像', upload_to='upload/mugshots', blank=True)
     created_time = models.DateTimeField('创建时间', default=now)
     last_mod_time = models.DateTimeField('修改时间', default=now)
+    home_address = models.CharField('家庭住址',max_length=200,blank=True)
 
     # objects = BlogUserManager()
 
@@ -25,3 +27,9 @@ class BlogUser(AbstractUser):
         site = get_current_site().domain
         url = "https://{site}{path}".format(site=site, path=self.get_absolute_url())
         return url
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        from_email = settings.DEFAULT_FROM_EMAIL
+        super(BlogUser, self).email_user(subject, message, from_email, **kwargs)
+        # send_mail(subject, message, from_email, [self.email], **kwargs)
